d4fba66935bfd993464a598c2dbff19ca6b00316
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-06 13:34:40 +0100

    Migraciones arregladas Citas2 creada Creados seeders restantes Seeders user listos! (11 columnas de datos) Dudas Damián Seeders (Enfermedads)

--- database/seeds/EnfermedadsTableSeeder.php
+++ database/seeds/EnfermedadsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class EnfermedadsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +12,15 @@ class EnfermedadsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('enfermedads')->insert([
+            'nombre' => Str::random(10),
+            'id' => '0',
+            'id_especialidad'=>'0',
+            /** Como se asignan valores a id y foreign keys?
+             *  A qué columnas hay que propocionar seeds?
+             * @todo
+             */
+
+        ]);
     }
 }
