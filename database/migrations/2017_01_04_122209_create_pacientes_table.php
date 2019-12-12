f4eaa99d884f86be6d6e56ebd1ccba5e75a7b581
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-12 13:08:26 +0100

    Migraciones arregladas Citas2 creada Creados seeders restantes Seeders user listos! (11 columnas de datos) Dudas Damián Seeders (Enfermedads)

--- database/migrations/2017_01_04_122209_create_pacientes_table.php
+++ database/migrations/2017_01_04_122209_create_pacientes_table.php
@@ -15,8 +15,8 @@ class CreatePacientesTable extends Migration
     {
         Schema::create('pacientes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
+            $table->string('nombre');
+            $table->string('apellidos');
             $table->string('nuhsa');
             $table->unsignedInteger('enfermedad_id');
             $table->timestamps();
