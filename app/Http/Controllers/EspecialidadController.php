dbe86f6269d1312bbd7bd72d933785368bb2e8ba
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-12 12:40:51 +0100

    Migraciones arregladas Citas2 creada Creados seeders restantes Seeders user listos! (11 columnas de datos) Dudas Damián Seeders (Enfermedads)

--- app/Http/Controllers/EspecialidadController.php
+++ app/Http/Controllers/EspecialidadController.php
@@ -132,7 +132,7 @@ class EspecialidadController extends Controller
     public function destroyAll()
     {
         Especialidad::truncate();
-        flash('Todas las especialidades borradas correctamente');
+        flash('Todas las especialidads borradas correctamente');
 
         return redirect()->route('especialidads.index');
     }
