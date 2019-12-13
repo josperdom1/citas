b9e28bffaf392d629740a93b5c69b74cec4db86a
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-13 08:59:21 +0100

    Todos los fallos arreglados excepto uno en citas Los 15 minutos arreglados Fallo de "cita has to be after now" arreglado

--- resources/views/citas/create.blade.php
+++ resources/views/citas/create.blade.php
@@ -14,12 +14,12 @@
                         <div class="form-group">
                             {!! Form::label('fecha_hora', 'Fecha y hora de inicio de la cita') !!}
 
-                            <input type="datetime-local" id="fecha_hora" name="fecha_hora" class="form-control" value="{{Carbon\Carbon::now()->addMinute()->format('Y-m-d\Th:i')}}" />
+                            <input type="datetime-local" id="fecha_hora" name="fecha_hora" class="form-control" value="{{Carbon\Carbon::now()->addMinute()->format('Y-m-d\TH:i')}}" />
                         </div>
                         <div class="form-group">
                             {!! Form::label('fecha_fin', 'Fecha y hora de fin de la cita') !!}
 
-                            <input type="datetime-local" id="fecha_fin" name="fecha_fin" class="form-control" value="{{Carbon\Carbon::now()->addMinutes(15)->format('Y-m-d\Th:i')}}" />
+                            <input type="datetime-local" id="fecha_fin" name="fecha_fin" class="form-control" value="{{Carbon\Carbon::now()->addMinutes(15)->format('Y-m-d\TH:i')}}" />
                         </div>
 
                         <div class="form-group">
