b8b786ce08738bd12358460e4b69713f107725be
Berta <bertacondeapinaniz@gmail.com>
2019-12-17 09:20:38 +0100

    los 3 modelos nuevos

--- app/MedTratamiento.php
+++ app/MedTratamiento.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class MedTratamiento extends Model
 {
     //
+
+    protected $fillable = ['fecha_inicial', 'fecha_final', 'unidades', 'frecuencia', 'instruccion', 'tratamiento_id'];
+    public function tratamientos()
+    {
+        return $this->belongsTo('App\Tratamiento');
+    }
+
+
+    public function medicamentos()
+    {
+        return $this->belongsToMany('App\Medicamento');
+    }
 }
--- app/Tratamiento.php
+++ app/Tratamiento.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tratamiento extends Model
 {
-    protected $fillable = ['fecha_inicio', 'fecha_fin', 'descripcion', 'medicamento_id'];
+    protected $fillable = ['fecha_inicio', 'fecha_fin', 'descripcion', 'medtratamiento_id'];
 
     public function medicamento()
     {
         return $this->hasMany('App\Medicamento');
+    }
+    public function medTratamiento()
+    {
+        return $this->hasMany('App\MedTratamiento');
     }
 
     protected $dates = ['fecha_inicio', 'fecha_fin'];
