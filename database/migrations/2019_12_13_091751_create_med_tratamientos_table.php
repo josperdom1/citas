8db5752694694de098a607165a770c4138d9bfd0
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-18 09:13:31 +0100

    fails solved v2

--- database/migrations/2019_12_13_091751_create_med_tratamientos_table.php
+++ database/migrations/2019_12_13_091751_create_med_tratamientos_table.php
@@ -15,11 +15,11 @@ class CreateMedTratamientosTable extends Migration
     {
         Schema::create('med_tratamientos', function (Blueprint $table) {
             $table->increments('id');
-            $table->increments('fecha_inicial');
-            $table->increments('fecha_final');
-            $table->increments('unidades');
-            $table->increments('frecuencia');
-            $table->increments('instruccion');
+            $table->dateTime('fecha_inicial');
+            $table->dateTime('fecha_final');
+            $table->integer('unidades');
+            $table->integer('frecuencia');
+            $table->string('instruccion');
             $table->unsignedInteger('tratamiento_id');
             $table->foreign('tratamiento_id')->references('id')->on('tratamientos')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
