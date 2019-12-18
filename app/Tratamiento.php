21d10f3208b62a48f15d2c88fdb4b8f2304debcc
Berta <bertacondeapinaniz@gmail.com>
2019-12-18 08:36:43 +0100

    los 3 migraciones nuevos

--- app/Tratamiento.php
+++ app/Tratamiento.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tratamiento extends Model
 {
-    protected $fillable = ['fecha_inicio', 'fecha_fin', 'descripcion'];
+    protected $fillable = ['nombre', 'fecha_inicio', 'fecha_fin', 'descripcion'];
 
     public function medicamento()
     {
