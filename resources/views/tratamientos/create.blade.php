5cb1bccb6e54e2074a1212bfc16463e0091cee83
abrahamvaquero <46657306+abrahamvaquero@users.noreply.github.com>
2019-12-18 09:04:54 +0100

    fails solved v2

--- resources/views/tratamientos/create.blade.php
+++ resources/views/tratamientos/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Crear tratamientoo</div>
+                    <div class="panel-heading">Crear tratamiento</div>
 
                     <div class="panel-body">
                         @include('flash::message')
@@ -13,9 +13,9 @@
                         {!! Form::open(['route' => 'tratamientos.store']) !!}
 
                         <div class="form-group">
-                            {!! Form::label('fecha_ini', 'Fecha de inicio del tratamiento') !!}
+                            {!! Form::label('fecha_inicio', 'Fecha de inicio del tratamiento') !!}
 
-                            <input type="datetime-local" id="fecha_ini" name="fecha_ini" class="form-control" value="{{Carbon\Carbon::now()->addMinute()->format('Y-m-d')}}" />
+                            <input type="datetime-local" id="fecha_inicio" name="fecha_inicio" class="form-control" value="{{Carbon\Carbon::now()->addMinute()->format('Y-m-d')}}" />
                         </div>
 
                         <div class="form-group">
